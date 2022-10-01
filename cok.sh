b95e5b965e1820bb72719f9e66320c3f6cf079b2
Ratcher98 <114542596+Ratcher98@users.noreply.github.com>
2022-10-01 19:15:33 +0700

    Update cok.sh

--- cok.sh
+++ cok.sh
@@ -10,7 +10,7 @@ echo "1. ketik 1 untuk lanjut"
 read -p "Pilih : "  ph
 #Cek input pengguna
 case $ph in
- 1)hasil=`echo $./nikto -url+$B | bc `
-;;
+ 1)hasil=`echo ./nikto -url+$B | bc `
+
 esac
 
