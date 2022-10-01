858781205bf03fadf061e3dd06b4105fbc87ad88
Ratcher98 <114542596+Ratcher98@users.noreply.github.com>
2022-10-01 19:07:40 +0700

    Add files via upload

---
+++ cok.sh
@@ -0,0 +1,16 @@
+clear
+figlet "Ratcher" | lolcat
+toilet -f standard "Morningstar" -F gay
+date | lolcat
+#Input angka yang ingin digunakan
+read -p "Masukkan link : " B
+#Input operasi yang di inginkan
+echo "Pilihan operasi"
+echo "1. ketik 1 untuk lanjut"
+read -p "Pilih : "  ph
+#Cek input pengguna
+case $ph in
+ 1)hasil=`echo $./nikto -url+$B | bc `
+;;
+esac
+
